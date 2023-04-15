ecf5a998a335e806cda4d699892d77ebe3cda1aa
hyena <mesutkee@gmail.com>
2023-04-15 12:27:55 +0630

    added new author and book

--- rest_apis/apiView/models.py
+++ rest_apis/apiView/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-genderC = (('male','female'),('female','female'))
+genderC = (('male','male'),('female','female'))
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
