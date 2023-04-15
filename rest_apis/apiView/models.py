e230b1680e4c2ad1e03bd487a342582a39d70e44
hyena <mesutkee@gmail.com>
2023-04-15 11:13:02 +0630

    migrated my pure app adding model register at admin site

--- rest_apis/apiView/models.py
+++ rest_apis/apiView/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+genderC = (('male','female'),('female','female'))
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    gender = models.CharField(max_length=100,choices=genderC)
+    
+    def __str__(self) -> str:
+        return self.first_name
+
+class Book(models.Model):
+    name = models.CharField(max_length=200)
+    publish_date = models.CharField(max_length=100)
+    book_type = models.CharField(max_length=200)
+    price = models.IntegerField()
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
 
 # Create your models here.
