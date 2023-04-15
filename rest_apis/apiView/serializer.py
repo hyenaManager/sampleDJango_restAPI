7677cefb98840718ed8e302267f782842b3162ab
hyena <mesutkee@gmail.com>
2023-04-15 17:52:13 +0630

    add new serializer for author in serializer

--- rest_apis/apiView/serializer.py
+++ rest_apis/apiView/serializer.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
-from .models import Book
+from .models import Book,Author
 
 class Book_serializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ('__all__')
 
-        
+class Author_serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ('__all__')
