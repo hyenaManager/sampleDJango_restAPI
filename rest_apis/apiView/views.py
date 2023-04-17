e2ba844ce8ae8a343fa02d2533f9f06f80d6c0e2
hyena <mesutkee@gmail.com>
2023-04-17 16:24:07 +0630

    modified in views.py and create new author

--- rest_apis/apiView/views.py
+++ rest_apis/apiView/views.py
@@ -2,20 +2,21 @@ from django.shortcuts import render
 from rest_framework.views import APIView  
 from rest_framework.response import Response  
 from rest_framework import status  
-from .models import Book 
-from .serializer import Book_serializer  
+from .models import Book,Author
+from .serializer import Book_serializer,Author_serializer 
 from rest_framework.decorators import api_view
 
 @api_view(['GET'])
 def getData(request):
-    book = Book.objects.all()
-    serializer = Book_serializer(book,many=True)
+    book = Author.objects.all()
+    serializer = Author_serializer(book,many=True)
     return Response(serializer.data)
 
 @api_view(['POST'])
 def createData(request):
-    book = request.data
-    serializer = Book_serializer(book)
+    author = request.data
+    serializer = Author_serializer(data=author)
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
+    
