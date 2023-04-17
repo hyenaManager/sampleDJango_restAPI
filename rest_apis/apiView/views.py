b777672fb9ee8daed2b586338a1585e3eb68a85c
hyena <mesutkee@gmail.com>
2023-04-17 15:22:20 +0630

    tryin sample view api

--- rest_apis/apiView/views.py
+++ rest_apis/apiView/views.py
@@ -6,17 +6,8 @@ from .models import Book
 from .serializer import Book_serializer  
 from rest_framework.decorators import api_view
 
-class BookView(APIView):
-
-    def get(self,request,*args,**kwargs):
-        result = Book.objects.all()
-        serializer = Book_serializer(result,many=True)
-        return Response({'status': 'success', "books":serializer.data}, status=200) 
-    
-    def post(self,request):
-        serializer1 = Book_serializer(data=request.data)  
-        if serializer1.is_valid():  
-            serializer1.save()  
-            return Response({"status": "success", "data": serializer1.data}, status=status.HTTP_200_OK)  
-        else:  
-            return Response({"status": "error", "data": serializer1.errors}, status=status.HTTP_400_BAD_REQUEST) 
+@api_view(['GET'])
+def get(request):
+    book = Book.objects.all()
+    serializer = Book_serializer(book,many=True)
+    return Response(serializer)
