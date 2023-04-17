6d20322ea281da18b5096d7700a3f2295cea868d
hyena <mesutkee@gmail.com>
2023-04-17 16:06:21 +0630

    create new function creatData in view and small changes in url

--- rest_apis/apiView/urls.py
+++ rest_apis/apiView/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('basic/', get),
+    path('get/', getData),
+    path('create/',createData)
 ]
--- rest_apis/apiView/views.py
+++ rest_apis/apiView/views.py
@@ -7,7 +7,15 @@ from .serializer import Book_serializer
 from rest_framework.decorators import api_view
 
 @api_view(['GET'])
-def get(request):
+def getData(request):
     book = Book.objects.all()
     serializer = Book_serializer(book,many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def createData(request):
+    book = request.data
+    serializer = Book_serializer(book)
+    if serializer.is_valid():
+        serializer.save()
     return Response(serializer.data)
