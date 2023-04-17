73bdc436b56e7c7a2f13a33e68babca12c8a4ac9
hyena <mesutkee@gmail.com>
2023-04-17 15:16:41 +0630

    small changes

--- rest_apis/apiView/views.py
+++ rest_apis/apiView/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status  
 from .models import Book 
 from .serializer import Book_serializer  
+from rest_framework.decorators import api_view
 
 class BookView(APIView):
 
